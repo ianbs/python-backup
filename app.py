27b5e515dc8f7c3a9da36b133f9f505311eabedf
Ian B. S <ianbarbosasantos@gmail.com>
2021-05-13 15:20:57 -0300

    feat: Setting up graphical user interface

--- app.py
+++ app.py
@@ -1,33 +1,51 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Entry, Button
+from tkinter.ttk import Progressbar
+import shutil
 
-# from events.backup import copia_pastas
+from events.backup import copia_pastas
 
 
-class MyFirstGUI:
+class MainGui:
     def __init__(self, master):
         self.master = master
-        master.title("A simple GUI")
-        master.geometry("500x300")
+        master.title("Backup")
+        master.geometry("650x150")
+        master.rowconfigure(4, minsize=300, weight=1)
+        master.columnconfigure(3, minsize=500, weight=1)
 
-        # self.label_index = 0
-        # self.label_text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-        # self.label = Label(master, textvariable=self.label_text)
-        # self.label.bind("<Button-1>", self.cycle_label_text)
-        # self.label.pack()
+        self.label_origem = Label(
+            text="Digite o caminho de origem:",
+            master=master,
+        )
+        self.label_origem.grid(row=0, column=0, padx=5, pady=5)
 
-        # self.greet_button = Button(master, text="Greet", command=self.greet)
-        # self.greet_button.pack()
+        self.caminho_origem = Entry(width=70)
+        self.caminho_origem.grid(row=0, column=1, padx=5, pady=5)
 
-        # self.close_button = Button(master, text="Close", command=master.quit)
-        # self.close_button.pack()
+        self.label_destino = Label(
+            text="Digite o caminho de origem:",
+            master=master,
+        )
+        self.label_destino.grid(row=1, column=0, padx=5, pady=5)
 
-    def greet(self):
-        print("Greetings!")
+        self.caminho_destino = Entry(width=70)
+        self.caminho_destino.grid(row=1, column=1, padx=5, pady=5)
 
-    def cycle_label_text(self):
-        print("Greetings!")
+        self.realizar_button = Button(
+            master,
+            text="Realizar Copia de Arquivos",
+            command=lambda: copia_pastas(
+                self.caminho_origem.get(), self.caminho_destino.get()
+            ),
+            height=1,
+            width=50,
+        )
+        self.realizar_button.grid(row=2, column=1, padx=20, pady=20)
+
+        self.progresso_copia = Progressbar(master=master)
+        self.progresso_copia.grid(row=4, column=1, padx=20, pady=20)
 
 
 root = Tk()
-my_gui = MyFirstGUI(root)
+gui = MainGui(root)
 root.mainloop()
--- events/backup.py
+++ events/backup.py
@@ -1,27 +1,21 @@
 import shutil
 import pathlib
 from datetime import date
-#import logging
+from tkinter.messagebox import showerror
 
 DATA_ATUAL = date.today()
 
 
 def copia_pastas(pasta_origem: str, pasta_destino: str) -> bool:
     try:
+        if not pasta_origem and not pasta_destino:
+            return showerror(
+                "Falta informação",
+                "Preencha os campos do diretorio de origem e de destino.",
+            )
         pasta_origem = pathlib.Path(pasta_origem).absolute()
-        pasta_destino = pathlib.Path(
-            pasta_destino + '-' + str(DATA_ATUAL)).absolute()
-        shutil.copytree(pasta_origem, pasta_destino,
-                        symlinks=True)
-    except OSError:
+        pasta_destino = pathlib.Path(pasta_destino + "-" + str(DATA_ATUAL)).absolute()
+        shutil.copytree(pasta_origem, pasta_destino, symlinks=True)
+    except OSError as error:
+        showerror(error, error)
         raise OSError.strerror
-
-
-pasta_origem = str(
-    input("Digite o caminho do diretorio de ORIGEM:"))
-pasta_destino = str(input("Digite o caminho do diretorio de DESTINO:"))
-
-if copia_pastas(pasta_origem, pasta_destino):
-    print("Arquivos Copiados")
-else:
-    print("Erros")
