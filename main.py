7a9e6c70c03fc181addb2178b93d6f1c4948ddf3
Ian B. S <ianbarbosasantos@gmail.com>
2021-05-13 08:45:44 -0300

    feat: Creating e testing copia_pastas function

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import shutil
-import os
+import pathlib
 from datetime import date
+#import logging
 
-data_atual = date.today()
+DATA_ATUAL = date.today()
 
 
-def copia_pastas(pasta_origem, pasta_destino):
-    pasta_origem = pasta_origem.replace("\\", "/")
-    pasta_destino = pasta_destino.replace("\\", "/")+"-"+str(data_atual)
-    shutil.copytree(pasta_origem, pasta_destino, symlinks=True)
+def copia_pastas(pasta_origem: str, pasta_destino: str) -> bool:
+    try:
+        pasta_origem = pathlib.Path(pasta_origem).absolute()
+        pasta_destino = pathlib.Path(
+            pasta_destino + '-' + str(DATA_ATUAL)).absolute()
+        shutil.copytree(pasta_origem, pasta_destino,
+                        symlinks=True)
+    except OSError:
+        raise OSError.strerror
     return True
 
 
 pasta_origem = str(
-    input("Digite o caminho do diretorio de ORIGEM:").replace("\\", "/"))
-pasta_destino = str(pasta_origem.replace("\\", "/")+"-"+str(data_atual))
+    input("Digite o caminho do diretorio de ORIGEM:"))
+pasta_destino = str(input("Digite o caminho do diretorio de DESTINO:"))
 
 if copia_pastas(pasta_origem, pasta_destino):
     print("Arquivos Copiados")
