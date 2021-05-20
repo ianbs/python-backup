7959fdf4dcedaef5d844c16dbec6d9fd757b7958
Ian B. S <ianbarbosasantos@gmail.com>
2021-05-20 10:59:31 -0300

    feat: Implementing logging into backup function

--- events/backup.py
+++ events/backup.py
@@ -1,10 +1,19 @@
 import shutil
 import pathlib
 import time
+import os
+import logging
 from datetime import date
 from tkinter.messagebox import showerror
 
 DATA_ATUAL = date.today()
+
+
+def _copiar_com_log(src, dst):
+    logging.info("Working in %s" % src)
+    logging.info("Working in %s" % dst)
+    print(f"Copiando: {src} to {dst}")
+    shutil.copy2(src, dst)
 
 
 def copia_pastas(pasta_origem: str, pasta_destino: str, step) -> bool:
@@ -17,7 +26,9 @@ def copia_pastas(pasta_origem: str, pasta_destino: str, step) -> bool:
         pasta_origem = pathlib.Path(pasta_origem).absolute()
         pasta_destino = pathlib.Path(pasta_destino + "-" + str(DATA_ATUAL)).absolute()
         step
-        shutil.copytree(pasta_origem, pasta_destino, symlinks=True)
+        shutil.copytree(
+            pasta_origem, pasta_destino, symlinks=True, copy_function=_copiar_com_log
+        )
     except OSError as error:
         showerror(error, error)
         raise OSError.strerror
